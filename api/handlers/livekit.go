c89e85eef82801c5bab5cdd80e743c1a096aae26
Panos Orovas <panagiwtis.orovas@gmail.com>
2025-09-08 15:31:04 +0300

    refactor livekit token endpoint

--- api/handlers/livekit.go
+++ api/handlers/livekit.go
@@ -1,84 +1,69 @@
 package api
 
 import (
-	"log"
+	"encoding/json"
+	"io"
 	"net/http"
 
-	"server/internal/livekit"
+	"server/internal/config"
+	"server/internal/utils"
+
+	"github.com/livekit/protocol/auth"
 )
 
-func SessionHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal("Error handling websocket connection.")
-		return
-	}
-
-	defer conn.Close()
-
+func LivekitTokenHandler(w http.ResponseWriter, r *http.Request) {
 	roomID := r.PathValue("room_id")
 	if roomID == "" {
 		return
 	}
 
-	var connected bool
-	var sessionID string
-	var authMsg AuthMessage
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
 
-	// Cleanup on disconnect
-	defer func() {
-		connectionsMutex.Lock()
-		delete(connectionPool[roomID], authMsg.SessionID)
-		if len(connectionPool[roomID]) == 0 {
-			delete(connectionPool, roomID)
-		}
-		connectionsMutex.Unlock()
-	}()
-
-	for {
-		var msg Message
-		if err := conn.ReadJSON(&msg); err != nil {
-			break
-		}
-
-		if !connected {
-			sessionID = msg.SessionID
-			connected = true
-
-			connectionsMutex.Lock()
-			if connectionPool[roomID] == nil {
-				connectionPool[roomID] = make(map[string]*Connection)
-			}
-			connectionPool[roomID][sessionID] = &Connection{Socket: conn}
-			connectionsMutex.Unlock()
-
-			livekitToken, err := livekit.CreateLivekitToken(roomID, sessionID)
-			if err != nil {
-				break
-			}
-
-			conn.WriteJSON(Message{
-				Type:        "livekit_session_token",
-				SessionID:   sessionID,
-				Token:       livekitToken,
-				Description: "User connected to room",
-			})
-
-			continue
-		}
-
-		if msg.To == "" {
-			connectionsMutex.Lock()
-			target := connectionPool[roomID][msg.SessionID]
-			connectionsMutex.Unlock()
-
-			if target != nil {
-				target.Send(msg)
-			}
-		}
+	var requestBody struct {
+		SessionID string `json:"session_id"`
 	}
 
-	connectionsMutex.Lock()
-	delete(connectionPool[roomID], sessionID)
-	connectionsMutex.Unlock()
+	err = json.Unmarshal(body, &requestBody)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	sessionID := requestBody.SessionID
+	if sessionID == "" {
+		http.Error(w, "missing session_id", http.StatusBadRequest)
+		return
+	}
+
+	livekitToken, err := createLivekitToken(roomID, sessionID)
+	if err != nil {
+		http.Error(w, "failed to create livekit token", http.StatusInternalServerError)
+		return
+	}
+
+	utils.JSONResponse(w, livekitToken, http.StatusOK)
+}
+
+func createLivekitToken(roomID, sessionID string) (string, error) {
+	config, err := config.LoadConfig()
+	if err != nil {
+		return "", err
+	}
+
+	LIVEKIT_API_KEY := config.Livekit.ApiKey
+	LIVEKIT_API_SECRET := config.Livekit.ApiSecret
+
+	token := auth.NewAccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET)
+	token.SetIdentity(sessionID)
+	token.SetVideoGrant(&auth.VideoGrant{
+		RoomJoin: true,
+		Room:     roomID,
+	})
+
+	return token.ToJWT()
 }
